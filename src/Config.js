aa99181d87753ee2b032a890c7f7ad33214bae53
k-abhishek <abhishek@deepcompute.com>
2019-02-11 18:34:41 +0530

    added reading env variables, configuring webpack and env js

---
+++ src/Config.js
@@ -0,0 +1,9 @@
+const config = {
+    appName: 'React Starter Kit',
+    homepage: '/',
+    primaryColor: '#88c1f9',
+    nferxUrl: typeof window === 'undefined' || window.location.hostname === 'localhost' ? '' : '//' + window.location.hostname
+  }
+export default config
+export { config }
+  
